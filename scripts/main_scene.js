c043ef3a97cda4a0ff2c8374ced08b2425c7f882
kuboi0627 <t.kuboi.sys22@morijyobi.ac.jp>
2023-12-19 14:18:06 +0900

    TaroとHanakoの表示

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -10,12 +10,19 @@ class MainScene extends Phaser.Scene {
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/Untitled.png');
+        this.load.image('Taro', 'assets/Taro.png');
+        this.load.image('Hanako', 'assets/Hanako.png')
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'background');
+        const Taro = this.physics.add.sprite(50, 50, 'Taro');
+        const Hanako = this.physics.add.sprite(700, 400, 'Hanako');
+
+        this.Taro = Taro
+        this.Hanako = Hanako
         }
 
      // 毎フレーム実行される繰り返し処理
